57085ebfe2f487f2f6a2b604ccbcaba0fee2a242
Leon Cheung <LWC@Leons-MacBook-Pro.local>
2019-03-02 16:37:48 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,7 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-  
-  socket.emit('createMessage', {
-    from: 'Hongqian',
-    text: 'Hello, this is Hongqian.'
-  });
-});
+});  
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
