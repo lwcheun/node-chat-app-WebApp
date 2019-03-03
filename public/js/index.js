910023e84ae2d913bfd9c2a7338c35a8b36ab409
Leon Cheung <LWC@Leons-MacBook-Pro.local>
2019-03-02 21:18:05 -0500

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,13 @@ jQuery('#message-form').on('submit', function (e) {
   // override default behavior
   e.preventDefault();
   
+  var messageTextbox = jQuery('[name=message]')
+  
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-    
+    jQuery('[name=message]').val('')	// to clear
   });
 });
 
@@ -45,13 +47,17 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
   
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+  
   navigator.geolocation.getCurrentPosition(function (position) {
     //console.log(position);
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
